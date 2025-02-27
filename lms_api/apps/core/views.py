05f9a1f4c8cc260d414240b78fc238d398c00d6b
Pradeepa <pradeepa@creatit.com.au>
2025-02-27 22:59:14 +0530

    typo.

--- lms_api/apps/core/views.py
+++ lms_api/apps/core/views.py
@@ -3,8 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
-from lms_api.apps.core import serializers, models
-from lms_api.apps.core.resources.services import get_tokens_for_user
+from lms_api.apps.core import serializers
 
 
 @api_view(["POST"])
