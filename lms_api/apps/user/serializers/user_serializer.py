0421c24a9b0eae690e0f235c3b0e5203ac677f67
Pradeepa Lakruwan <pradeepa@creatit.com.au>
2025-05-24 23:07:18 +0530

    Added user create func to Admin role.

--- lms_api/apps/user/serializers/user_serializer.py
+++ lms_api/apps/user/serializers/user_serializer.py
@@ -5,6 +5,16 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
     class Meta:
         model = apps.get_model(settings.AUTH_USER_MODEL)
-        fields = ["id", "username", "email", "first_name", "last_name", "role", "is_verified"]
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "role",
+            "password",
+        ]
--- lms_api/apps/user/views.py
+++ lms_api/apps/user/views.py
@@ -3,7 +3,8 @@ from django.apps import apps
 
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 from lms_api.apps.user.serializers import UserSerializer
 from lms_api.resources.enum.user_enum import UserRoleEnum
@@ -36,4 +37,16 @@ class AdminUserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [IsAdminRole]
     queryset = apps.get_model(settings.AUTH_USER_MODEL).objects.filter(is_verified=True)
-    http_method_names = ["get", "delete"]
+    http_method_names = ["get", "delete", "post"]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+
+        user = serializer.save()
+        user.set_password(validated_data["password"])
+        user.save()
+
+        response_serializer = self.get_serializer(user)
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
